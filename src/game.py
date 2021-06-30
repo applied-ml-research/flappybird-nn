84db57e44f3baeffece6ec8805234db9e7ee80e8
thisistrivial <g.lee13770334@gmail.com>
2021-06-29 22:13:52 -0400

    Replaced pole constructor parameters with constants

--- src/game.py
+++ src/game.py
@@ -1,11 +1,15 @@
 import random
 
-BIRD_X = 50
+WIDTH = 800
+HEIGHT = 600
+
+BIRD_X = 100
 BIRD_START_Y = 300
 GRAVITY = 0.8
 
 POLE_HOLE_HEIGHT = 100
-POLE_SPAWN_X = 800
+POLE_MIN_BOT = 0
+POLE_MAX_TOP = 800
 
 class Bird:
   def __init__(self):
@@ -15,9 +19,9 @@ class Bird:
     self.y_velocity = 0
 
 class Pole:
-  def __init__(self, min_bot, max_top):
+  def __init__(self):
     try:
-      self.hole_top = random.randint(min_bot + POLE_HOLE_HEIGHT, max_top)
+      self.hole_top = random.randint(POLE_MIN_BOT + POLE_HOLE_HEIGHT, POLE_MAX_TOP)
     except ValueError as e:
-      raise Exception("Pole creation failed: difference between max_top and min_bot is less than POLE_HOLE_HEIGHT").with_traceback(e.__traceback__) from None
-    self.x = POLE_SPAWN_X
+      raise Exception("Pole creation failed: difference between POLE_MAX_TOP and POLE_MIN_BOT is less than POLE_HOLE_HEIGHT").with_traceback(e.__traceback__) from None
+    self.x = WIDTH
