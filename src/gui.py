d959c5a6c4ccb06a3304d92e97737ed3509106fa
thisistrivial <g.lee13770334@gmail.com>
2021-06-30 09:45:41 -0400

    Added rendering aesthetics

--- src/gui.py
+++ src/gui.py
@@ -5,21 +5,10 @@ import tkinter
 FPS = 30
 EVERY = 1000//FPS
 
+BIRD_COLOR = 'black'
+
 space_pressed = False
-
-def update(root, canvas, gm):
-  canvas.delete('all')
-  global space_pressed
-  if not gm.update(space_pressed):
-    gm = game.Game() 
-
-  bird = gm.bird
-  canvas.create_oval(bird.x - bird.radius, bird.y - bird.radius, bird.x + bird.radius, bird.y + bird.radius)
-
-  for pole in gm.poles:
-    canvas.create_line(pole.x, pole.hole_top, pole.x, 0)
-    canvas.create_line(pole.x, pole.hole_top + game.POLE_HOLE_HEIGHT, pole.x, game.HEIGHT)
-  root.after(EVERY, update, root, canvas, gm) 
+canvas_buffer = []
 
 def main():
   root = tkinter.Tk()
@@ -31,6 +20,23 @@ def main():
   root.bind('<KeyRelease-Return>', handleKeyRelease)
   update(root, canvas, gm)
   root.mainloop()
+
+def update(root, canvas, gm):
+  global space_pressed
+  global canvas_buffer
+  if not gm.update(space_pressed):
+    gm = game.Game() 
+
+  next_buffer=[]
+  bird = gm.bird
+  next_buffer.append(canvas.create_oval(bird.x - bird.radius, bird.y - bird.radius, bird.x + bird.radius, bird.y + bird.radius, fill=BIRD_COLOR))
+
+  for pole in gm.poles:
+    next_buffer.append(canvas.create_line(pole.x, pole.hole_top, pole.x, 0))
+    next_buffer.append(canvas.create_line(pole.x, pole.hole_top + game.POLE_HOLE_HEIGHT, pole.x, game.HEIGHT))
+  canvas.delete(*canvas_buffer)
+  canvas_buffer = next_buffer
+  root.after(EVERY, update, root, canvas, gm) 
 
 def handleKeyPress(event):
   global space_pressed
